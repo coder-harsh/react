8f0ad1038161ed89cf0e7a8a41b442b718613851
Harsh <harshj7.net@gmail.com>
2023-04-08 00:01:29 +0530

    add all react-file-structure till line no 79

---
+++ js/5-react-file-structure.js
@@ -0,0 +1,79 @@
+/*
+ package k gitignore m depandencies ko ignore kiya jata hain
+
+ readme se github documentation
+
+
+ package.json and package-lock.json is heart of node package
+
+ npm_module m react ka file rahta hain.
+
+ remember 2 file react and react dom in node_module
+
+
+ command run p package.json, package-lock/json and node_module ko auto update karta hain.
+
+
+ only need of 2 folder public and src
+
+ sbse jyda src use hoga.
+
+
+
+ public folder ka mainfest json m kuchh nhi karenge. only used for progressive web app(pwa)
+
+
+ pwa work like app.
+
+ main h index.html
+
+
+ react se hum spa banate hain. yahi index.html ek page h only.
+
+
+ baki page page nhi hota. only component add hoke page ban jata h.
+
+<div id="root"></div>
+ isi div m react component add karta hain.
+
+
+ public folder kabhi kabhi jarurat padta hain.
+
+
+
+ <script defer src="/static/js/bundle.js"></script> yahi script component add karta hain.
+
+
+ source code m sirf wahi file dikhta hain jo server se aaya h baki js ne add kiya hain.
+
+
+
+ sbse main src folder hain.
+
+ src: source
+
+
+ setup.js nd setup.test.js testing k liye hain.
+
+
+ reportwebvital report generte karne k liye hain.
+
+
+
+
+ src ka logo.svg h hero wala img 
+
+
+ hum react m img,css v import kar sakte hain.
+
+ reactapp src wala index.js se start hota hain.
+
+ aur index.css universal website k liye hain. aisa css jo pure website k liye same rahe, use hum index.css m rakhte hain.
+
+
+component 2 tarike se bana sakte hain. ek function se ek class se. jydatar function se component banta hain.
+
+function k andr hum jsx ka code html jaisa likhte hain isse hum componnet kahte hain.
+
+jsx is mixture of html and javascript.
+*/
