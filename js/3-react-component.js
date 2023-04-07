445e380c8b67ada7e63ffb673173ec8cf61fcd3f
Harsh <harshj7.net@gmail.com>
2023-04-07 17:09:18 +0530

    add all 3-react-component till line no 13

---
+++ js/3-react-component.js
@@ -0,0 +1,13 @@
+/*
+react is all about component
+
+we can create 1 component with 1 tag or we can create a component with a group of 5-6 tag
+
+
+Component are reusable building blocks in user ineterface(html css and js makes a component).
+
+in react we build component with html css js.
+
+
+and then by combining these component we built a page.
+*/
