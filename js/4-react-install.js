d7b250954f29157af4f9dc4d79c18dc2a3719166
Harsh <harshj7.net@gmail.com>
2023-04-07 19:15:34 +0530

    add all 4-react-install till line no 59

---
+++ js/4-react-install.js
@@ -0,0 +1,59 @@
+/*
+node js is an open source cross platform.
+
+pahle js only browser p chalta tha.
+it provides run time environemt to js to run locally on computer. if js will run on compueter, then we can use js in server. so we use node js in backend for server too.
+
+jo js browser p run ho use hum javascript bolte hain.
+jo js locally compuetr p run ho use hum node js bolte hain.
+
+
+react app/website k liye node js ka jarurat h.
+
+
+
+check node.js install
+
+go to cmd
+
+command:node  //Welcome to Node.js v18.15.0.
+
+all js kam krega. kuch browser wala chhor k like alert, console.js
+
+press ctrl+c 2 times to exit from cmd.
+
+run npm and npx in cmd to install depandencies.
+
+
+
+npm: a website for js package manager which manage depandencies.
+
+
+npm: pacakge manager for js programming language.
+
+
+facebook has created react on node. unhone direct hume download karne k liye link nhi diya but fb ne react app develope karne k liye jo file banayi usko server p pacakge bana k daal diya. wo package hi npm p h. jb bhi code fb update karta hain npm p krta hain. npm hume command deta hain jisse hum npm server se humare computer p le aate hain.
+
+koi v package jo npm p rakha hain use apne computer p lane hetu hum npm ya npx run karte hain.
+
+
+ek command se react ka basic setup compuetr m download ho jayega.
+
+us folder m cmd jake run karna h ye command
+npx create-react-app techgun
+
+
+uske bad use hum npm start se basic app/website browser me local server k through open hoga.
+
+ab hum apna kam start kar sakte hain.
+
+ye server local h.http://localhost:3000/ no need to refresh. hume ye server start karna padega. 
+ek network server bhi milega phone k liye. http://192.168.136.63:3000 same wifi k liye
+
+
+
+server band karne k liye ctrl+c kareneg terminal m.
+
+
+server start karne k liye npm start
+*/
